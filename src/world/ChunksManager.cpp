6e05ec6d251ef7deb18c580c420b4d3fb4bdba8c
ToropovAlexey <henschel37rus@gmail.com>
2024-10-26 16:35:29 +0300

    cmake updated

--- src/world/ChunksManager.cpp
+++ src/world/ChunksManager.cpp
@@ -1,11 +1,11 @@
 #include "ChunksManager.hpp"
-#include <tracy/Tracy.hpp>
 #include <algorithm>
 #include <execution>
+#include <future>
 #include <memory>
+#include <tracy/Tracy.hpp>
 #include <tuple>
 #include <vector>
-#include <future>
 
 ChunksManager::ChunksManager(BlocksManager &blocksManager,
                              TextureAtlas &textureAtlas, int playerX,
@@ -15,37 +15,28 @@ ChunksManager::ChunksManager(BlocksManager &blocksManager,
   ZoneScoped;
   setPlayerPos(playerX, playerZ);
 
-
   std::vector<std::tuple<int, int>> chunksToGenerate;
-  for (int x = m_playerX - 24; x <= m_playerX + 24; x++) {
-    for (int z = m_playerZ - 24; z <= m_playerZ + 24; z++) {
+  const int radius = 40;
+  for (int x = m_playerX - radius; x <= m_playerX + radius; x++) {
+    for (int z = m_playerZ - radius; z <= m_playerZ + radius; z++) {
       chunksToGenerate.emplace_back(x, z);
     }
   }
 
   std::vector<std::future<void>> futures;
 
-  for (const auto& chunkPos : chunksToGenerate) {
+  for (const auto &chunkPos : chunksToGenerate) {
     futures.emplace_back(std::async(std::launch::async, [this, &chunkPos]() {
-      auto chunk = m_worldGenerator.generateChunk(
-        std::get<0>(chunkPos), std::get<1>(chunkPos));
+      auto chunk = m_worldGenerator.generateChunk(std::get<0>(chunkPos),
+                                                  std::get<1>(chunkPos));
       chunk->generateVerticesAndIndices();
       this->insertChunk(chunk);
-      }));
+    }));
   }
 
-  for (auto& fut : futures) {
+  for (auto &fut : futures) {
     fut.get();
   }
-    
-
-  // m_taskflow.for_each(chunksToGenerate.begin(), chunksToGenerate.end(),
-  //                     [this](std::tuple<int, int> &chunkPos) {
-  //                       this->insertChunk(m_worldGenerator.generateChunk(
-  //                           std::get<0>(chunkPos), std::get<1>(chunkPos)));
-  //                     });
-
-  // m_executor.run(m_taskflow).wait();
 }
 
 void ChunksManager::setPlayerPos(int x, int z) {
